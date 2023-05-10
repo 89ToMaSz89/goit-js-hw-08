be4f631712a1db6754981a81d5cd5e2137693538
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-05-10 21:07:25 +0200

    Done task 01

---
+++ src/js/01-gallery.js
@@ -0,0 +1,32 @@
+// Add imports above this line
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+galleryItems.forEach(event => {
+
+    const galleryHtml = `
+        <li>
+            <a class = "gallery__item" href = "${event.original}">
+                <img 
+                    class = "gallery__image" 
+                    src = "${event.preview}"
+                    alt = "${event.description}">
+            </a>
+        </li>`
+
+    galleryList.insertAdjacentHTML('beforeend', galleryHtml);
+});
+
+const newGallery = new simpleLightbox('.gallery a' , {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom'
+});
