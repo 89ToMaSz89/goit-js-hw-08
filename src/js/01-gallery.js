75de749f6afb0f67ed7d023414e7bde43c81c5d9
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-05-10 21:12:21 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,6 +6,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(simpleLightbox);
 
 const galleryList = document.querySelector('.gallery');
 
