11b86661c9d7ddb1b5af10dad399be87fc0e3c62
Tomasz Borek <tomasz.borek.89@gmail.com>
2023-05-13 23:43:57 +0200

    Task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
 
+
+const iframe = document.querySelector('vimeo-player');
+const nplayer = new Vimeo.Player(iframe);
+console.log(player);
+
+nplayer.on('play', function() {
+    console.log('played the video!');
+});
+
+nplayer.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
