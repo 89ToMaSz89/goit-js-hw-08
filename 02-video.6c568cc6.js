ca3e136392faed7af7b158c73d59e3191fa76865
89ToMaSz89 <111086476+89ToMaSz89@users.noreply.github.com>
2023-05-13 21:44:22 +0000

    Deploying to gh-pages from @ 89ToMaSz89/goit-js-hw-08@11b86661c9d7ddb1b5af10dad399be87fc0e3c62 🚀

---
+++ 02-video.6c568cc6.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("vimeo-player"),o=new Vimeo.Player(e);console.log(player),o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
+//# sourceMappingURL=02-video.6c568cc6.js.map
---
+++ 02-video.ec18a1e1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("vimeo-player"),o=new Vimeo.Player(e);console.log(player),o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
+//# sourceMappingURL=02-video.ec18a1e1.js.map
